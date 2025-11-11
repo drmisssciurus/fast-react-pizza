1aee7bbefdb921055666abc3163ecd128381b943
Alina <ms.sciurus@gmail.com>
2025-11-11 19:00:57 +0200

    global state; redux toolkit

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,12 @@
+import { useSelector } from 'react-redux';
+
 function Username() {
-  return <div className='mb:block hidden text-sm font-semibold'>Alina</div>;
+  const username = useSelector((state) => state.user.username);
+  if (!username) return null;
+  return (
+    // <div className='mb:block hidden text-sm font-semibold'>{username}</div>
+    <div>{username}</div>
+  );
 }
 
 export default Username;
